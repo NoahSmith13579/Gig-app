50b8ce394ba26f0d4223c111a9aa3967e7b0c78a
Noah Smith <noahbsmith13579@gmail.com>
2023-07-03 10:13:23 -0500

    Changed href for some links

--- time-ui/src/components/NavbarHeader.tsx
+++ time-ui/src/components/NavbarHeader.tsx
@@ -4,41 +4,41 @@ import LoginButton from './loginButton';
 import LogoutButton from './LogoutButton';
 
 const NavbarHeader: React.FC = () => {
-    const { authState: auth } = useAuth();
+  const { authState: auth } = useAuth();
 
-    const loggedIn = auth.loggedIn;
+  const loggedIn = auth.loggedIn;
 
-    return (
-        <nav>
-            <div className='flex'>
-                <span className='brand'>Gig app v2</span>
-            </div>
-            <div className='flex grow'>
-                <ul className='flex grow align-center'>
-                    <li>
-                        <a href='/dash'>Dashboard</a>
-                    </li>
-                    <li>
-                        <a href='/projects'>Projects</a>
-                    </li>
-                    <li>
-                        <a href='/about'>About</a>
-                    </li>
+  return (
+    <nav>
+      <div className='flex'>
+        <span className='brand'>Gig app v2</span>
+      </div>
+      <div className='flex grow'>
+        <ul className='flex grow align-center'>
+          <li>
+            <a href='/Gig-app/dash'>Dashboard</a>
+          </li>
+          <li>
+            <a href='/Gig-app/projects'>Projects</a>
+          </li>
+          <li>
+            <a href='/Gig-app/about'>About</a>
+          </li>
 
-                    {loggedIn ? (
-                        <>
-                            <li className='ml-auto'>
-                                <span>Hello {auth.name}</span>
-                            </li>
-                            <li>{<LogoutButton />}</li>
-                        </>
-                    ) : (
-                        <li className='ml-auto'>{<LoginButton />}</li>
-                    )}
-                </ul>
-            </div>
-        </nav>
-    );
+          {loggedIn ? (
+            <>
+              <li className='ml-auto'>
+                <span>Hello {auth.name}</span>
+              </li>
+              <li>{<LogoutButton />}</li>
+            </>
+          ) : (
+            <li className='ml-auto'>{<LoginButton />}</li>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavbarHeader;
--- time-ui/src/routes/Projects/components/ProjectCard.tsx
+++ time-ui/src/routes/Projects/components/ProjectCard.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import Project from '../../../entities/Project';
 
 interface ProjectCardProps {
-    project: Project;
+  project: Project;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
-    return (
-        <div className='card'>
-            <a href={`/projects/${project.id}`}>
-                <span className='title'>{project.name}</span>
-            </a>
-            <span className='subtitle'>{project.id}</span>
-            <span>{project.description}</span>
-            <span>
-                <b>
-                    <u>Owner</u>
-                </b>
-                : {project.owner}
-            </span>
-        </div>
-    );
+  return (
+    <div className='card'>
+      <a href={`/Gig-app/projects/${project.id}`}>
+        <span className='title'>{project.name}</span>
+      </a>
+      <span className='subtitle'>{project.id}</span>
+      <span>{project.description}</span>
+      <span>
+        <b>
+          <u>Owner</u>
+        </b>
+        : {project.owner}
+      </span>
+    </div>
+  );
 };
 
 export default ProjectCard;
